3b617d8fe28f6754955ad74df8cafbd8b78f269a
amarmahaseth <you@example.comamarmahaseth09@gmail.com>
2024-04-23 14:30:24 +0545

    homepage page 1st complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation (){
+    
 
 var tl = gsap.timeline()
 tl.from(".line h1", {
@@ -37,9 +38,32 @@ tl.from("#page1", {
     delay:0.2,
     y: 1600,
     opacity: 0,
-    duration: 0.5,
-    ease:power4
+    duration: 0.6,
+    // ease:power4
 })
 tl.to("#loader",{
     display:"none"
 })
+tl.from(".nav",{
+    opacity:0
+})
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1 ",{
+    y: 120,
+    stagger:0.2
+})
+
+}
+loadingAnimation();
+
+function cursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
